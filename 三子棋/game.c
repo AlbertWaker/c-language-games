0e10132b5af103aae2781b9d19c1690daf97b438
AlbertWaker <151443696+AlbertWaker@users.noreply.github.com>
2024-02-25 22:19:02 +0800

    Add files via upload

---
+++ 三子棋/game.c
@@ -0,0 +1,149 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "game.h"
+
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < ROW; i++)
+	{
+		for (j = 0; j < COL; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		//1.打印一行的数据
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ",board[i][j]);
+			if (j < col - 1)
+			{
+				printf("|");
+			}
+		}
+		printf("\n");
+		//2.打印分割行
+		if (i < row - 1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j < col - 1)
+				{
+					printf("|");
+				}
+			}
+			printf("\n");
+		}
+	}
+}
+
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("玩家走:>\n");
+	//判断x,y坐标的合法性
+	while (1)
+	{
+		printf("请输入要下的坐标:>");
+		scanf("%d%d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("该坐标被占用\n");
+			}
+		}
+		else
+		{
+			printf("坐标非法，请重新输入！\n");
+		}
+	}
+}
+
+void ComputerMove(char board[ROW][COL], int row, int col)
+{
+	int x;
+	int y;
+	printf("电脑走:>\n");
+	while (1)
+	{
+		x = rand() % row;
+		y = rand() % col;
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '#';
+			break;
+		}
+	}
+}
+
+//返回1表示棋盘满了
+//返回0表示棋盘没满
+int IsFull(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;//没满
+			}
+		}
+	}
+	return 1;//满了
+}
+
+char IsWin(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	//横三行
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
+		{
+			return board[i][1];
+		}
+	}
+	//竖三列
+	for (i = 0; i < row; i++)
+	{
+		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
+		{
+			return board[1][i];
+		}
+	}
+	//两个对角线
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	//判断是否平局
+	if (1 == IsFull(board, row, col))
+	{
+		return 'Q';
+	}
+	return 'C';
+}
